eca3fc49e71d4dd76ea25879be85529da3fb3f8b
stijn.buys <stijn.buys@gmail.com>
2019-06-12 15:52:08 +0200

    retrovideo

---
+++ src/main/java/be/vdab/retrovideo/services/DefaultGenreService.java
@@ -0,0 +1,29 @@
+package be.vdab.retrovideo.services;
+
+import be.vdab.retrovideo.domain.Genre;
+import be.vdab.retrovideo.repositories.GenreRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true , isolation = Isolation.READ_COMMITTED)
+public class DefaultGenreService implements GenreService
+{
+    private final GenreRepository genreRepository;
+
+    //CONSTRUCTORS
+    DefaultGenreService(GenreRepository genreRepository)
+    {
+        this.genreRepository = genreRepository;
+    }
+
+    //METHODS
+    @Override
+    public List<Genre> findAll()
+    {
+        return genreRepository.findAll();
+    }
+}
